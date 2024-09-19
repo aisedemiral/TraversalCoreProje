225fca70d0efcb2dae0794b8239e8985666b9517
Mümine Aişe Demiral <mumineaise@gmail.com>
2024-09-19 12:42:26 +0300

    Concrate içerisine Guide,Contact,About2 classları eklendi ve About classı güncellendi.

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,10 @@
+namespace EntityLayer.Concrete;
+
+public class Feature
+{
+    public int FeatureID { get; set; }
+    public string Title { get; set; }
+    public string PostDescription { get; set; }
+    public string PostImage { get; set; }
+    public bool Status { get; set; }
+}
