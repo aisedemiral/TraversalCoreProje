2d8c6a6997f12c9d5f9fffdfbb42869c6b0323c6
Mümine Aişe Demiral <mumineaise@gmail.com>
2024-09-23 13:58:00 +0300

    Identity kütüphanesi eklendi ve login , resigter sayfaları eklendi

---
+++ EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace EntityLayer.Concrete;
+
+public class AppUser:IdentityUser<int>
+{
+    public string ImageUrl { get; set; }
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string Gender { get; set; }
+}
