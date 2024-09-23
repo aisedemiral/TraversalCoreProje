178651b420cc9bc18259e1d17f8f592d8773304b
Mümine Aişe Demiral <mumineaise@gmail.com>
2024-09-23 16:56:01 +0300

    Custom Identity validator (hatalı parola girişlerinin türkçeleştirilmesi).

---
+++ TraversalCoreProje/Models/CustomIdentityValidator.cs
@@ -0,0 +1,43 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Identity;
+
+namespace TraversalCoreProje.Models;
+
+public class CustomIdentityValidator:IdentityErrorDescriber
+{
+    public override IdentityError PasswordTooShort(int length)
+    {
+        return new IdentityError()
+        {
+            Code = "PasswordToShort",
+            Description = $"Parola Minimum {length} karakter olmalıdır."
+        };
+    }
+
+    public override IdentityError PasswordRequiresUpper()
+    {
+        return new IdentityError()
+        {
+            Code = "PasswordRequiresUpper",
+            Description="Parola en az 1 küçük harf içermelidir."
+        };
+    }
+
+    public override IdentityError PasswordRequiresLower()
+    {
+        return new IdentityError()
+        {
+            Code = "PasswordRequiresLower",
+            Description = "Parola en az 1 küçük harf içermelidir."
+        };
+    }
+
+    public override IdentityError PasswordRequiresNonAlphanumeric()
+    {
+        return new IdentityError()
+        {
+            Code = "PasswordRequiresNonAlphanumeric",
+            Description = "Parola en az bir sembol içermelidir."
+        };
+    }
+}
