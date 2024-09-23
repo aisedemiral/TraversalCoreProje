60789ff72350391a2f53a238dc01ae093a593461
Mümine Aişe Demiral <mumineaise@gmail.com>
2024-09-23 16:57:24 +0300

    tema dosyaları ve migrationlar eklendi

---
+++ DataAccessLayer/Migrations/20240920132054_add_destination_details.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_destination_details : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImage",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details1",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details2",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image2",
+                table: "Destinations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Abouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverImage",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Details1",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Details2",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Image2",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Abouts");
+        }
+    }
+}
---
+++ TraversalCoreProje/Views/Login/SignUp.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TraversalCoreProje.Views.Login;
+
+public class SignUp : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
