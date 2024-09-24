40d81567e7d23992fa063b36cb6cfdd08d90059f
Mümine Aişe Demiral <mumineaise@gmail.com>
2024-09-24 18:50:01 +0300

    Member Profile Dashboard verilerinin düzenlenmesi listelenmesi

---
+++ TraversalCoreProje/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Member.Controllers;
+[Area("Member")]
+public class DashboardController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public DashboardController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var values = await _userManager.FindByNameAsync(User.Identity.Name);
+        ViewBag.username = values.Name + " " + values.SurName;
+        ViewBag.userImage = values.ImageUrl;
+        return View();
+    }
+}
