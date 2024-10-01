762d7497a3672108568b66ea2f1b8cef04f4eb3f
Mümine Aişe Demiral <mumineaise@gmail.com>
2024-10-01 17:33:52 +0300

    ajax işlemlerinin tamamı

---
+++ TraversalCoreProje/Models/CityClass.cs
@@ -0,0 +1,8 @@
+namespace TraversalCoreProje.Models;
+
+public class CityClass
+{
+    public int CityID { get; set; }
+    public string CityName { get; set; }
+    public string CityCountry { get; set; }
+}
