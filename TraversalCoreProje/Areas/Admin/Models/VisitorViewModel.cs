6e7767aec877919e5a290976c143584b96ffb1c8
Mümine Aişe Demiral <mumineaise@gmail.com>
2024-10-08 17:22:21 +0300

    Api Düzenlemeleri

---
+++ TraversalCoreProje/Areas/Admin/Models/VisitorViewModel.cs
@@ -0,0 +1,16 @@
+using System.util;
+
+namespace TraversalCoreProje.Areas.Admin.Models;
+
+public class VisitorViewModel
+{
+    public int VisitorID { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string Mail { get; set; }
+
+
+    
+}
